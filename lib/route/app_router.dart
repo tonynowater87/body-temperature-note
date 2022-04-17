9fb1e721b12ee05b2b37484ad3d2eadb6cd8b130
tonynowater <tonynowater87@gmai..com>
2022-04-17 22:12:43 +0800

    Added routes

---
+++ lib/route/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/annotations.dart';
+import 'package:body_temperature_note/views/chart_page.dart';
+import 'package:body_temperature_note/views/home_page.dart';
+import 'package:body_temperature_note/views/input_page.dart';
+import 'package:body_temperature_note/views/settings_page.dart';
+
+@MaterialAutoRouter(routes: <AutoRoute>[
+  AutoRoute(page: HomePage, initial: true),
+  AutoRoute<DateTime>(page: InputPage),
+  AutoRoute(page: ChartPage),
+  AutoRoute(page: SettingsPage),
+])
+class $AppRouter {}
