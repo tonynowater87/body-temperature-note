7ab4354a63e2f0a300045fa5738891f3d1a7c3f6
tonynowater <tonynowater87@gmai..com>
2022-04-23 19:16:02 +0800

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:body_temperature_note/firebase_options.dart';
 import 'package:body_temperature_note/route/app_router.gr.dart';
 import 'package:body_temperature_note/views/home/home_cubit.dart';
 import 'package:body_temperature_note/views/utils/app_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   BlocOverrides.runZoned(() {
     runApp(MyApp());
   }, blocObserver: AppBlocObserver());
